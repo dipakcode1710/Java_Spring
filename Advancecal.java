61e9671db2cb524b140574b14aa4a97ac941485c
dipakvitec <dipak.code1710@gmail.com>
2025-01-12 16:43:25 +0530

    Multi level inheritance
    Multi level inheritance

---
+++ Advancecal.java
@@ -0,0 +1,7 @@
+public class Advancecal extends Div
+{
+    public double precent(int n1, int n2)
+    {
+        return (n1 / n2) * 100;
+    }
+}
--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,16 @@
 public class Calculator{
     public static void main(String a[])
     {
-        Div ad = new Div();
+        Advancecal ad = new Advancecal();
         int r1 = ad.add(4 , 5);
         int r2 = ad.sub(10 , 5);
         int r3 = ad.Div(4 , 2);
         int r4 = ad.mult(4 , 5);
+        double r5 = ad.precent(100,520);
         System.out.println("Addition :" + r1);
         System.out.println("Substraction :" +r2);
         System.out.println("Division :" +r3);
         System.out.println("Multiplication :" +r4);
+        System.out.println("Precent :" +r4);
     }
 }
