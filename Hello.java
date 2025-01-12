62491551c66b4ddfaa78d4035879a785d5382836
dipakvitec <dipak.code1710@gmail.com>
2025-01-12 15:43:17 +0530

    convenstion
    convenstion

--- Hello.java
+++ Hello.java
@@ -1,45 +1,51 @@
 // Encapsulation 
 class Human
 {
-    private int age;
-    private String name;
+    // private int age;
+    // private String name;
 
-    // Construstor is a special method.
-    // It has same name as class name.
-    // Construstor never return anything.
-    // Every time we will create an object it will call construstor
-    public Human() //Default construstor
+    // // Construstor is a special method.
+    // // It has same name as class name.
+    // // Construstor never return anything.
+    // // Every time we will create an object it will call construstor
+    public Human()
     {
-        System.out.println("In construstor 1");
-        age = 11;
-        name= "Pradip";
+        System.out.println("Dipak");
     }
 
-    public Human(int age, String name) // parameterized construstor
-    {
-        System.out.println("In construstor 2");
-        this.age=age;
-        this.name=name;
-    }
+    // public Human(int age, String name) // parameterized construstor
+    // {
+    //     System.out.println("In construstor 2");
+    //     this.age=age;
+    //     this.name=name;
+    // }
     
-    public int getAge()
-    {
-        return age;
-    }
+    // public int getAge()
+    // {
+    //     return age;
+    // }
 
-    public String getName()
-    {
-        return name;
-    }
+    // public String getName()
+    // {
+    //     return name;
+    // }
 
-    public void setName(String a)
-    {
-        name = a;
-    }
+    // public void setName(String a)
+    // {
+    //     name = a;
+    // }
 
-    public void setAge(int n)
+    // public void setAge(int n)
+    // {
+    //     age = n;
+    // }
+}
+
+class Child extends Human
+{
+    public Child()
     {
-        age = n;
+        System.out.println("Pradip");
     }
 }
 
@@ -47,15 +53,23 @@ class Human
 public class Hello{
     public static void main(String a[])
     {
-        Human obj = new Human();
-        Human obj1 = new Human(17,"Shubham");
+        // Human obj = new Human();
+        Child bby = new Child();
+        // Human obj1 = new Human(17,"Shubham");
         
-        //obj.setName("Kavita");
-        //obj.setAge(42);
-        //String n = obj.getName();
-        //int b = obj.getAge();
+        // obj.setName("Kavita");
+        // obj.setAge(42);
+        // String n = obj.getName();
+        // int b = obj.getAge();
         
-        System.out.println(obj.getName() + " : " + obj.getAge() );
-        System.out.println(obj1.getName() + " : " + obj1.getAge() );
+        // System.out.println(obj.getName() + " : " + obj.getAge() );
+        // System.out.println(obj1.getName() + " : " + obj1.getAge() );
     }
 }
+
+// Name convenstion
+// java use camel casing
+// class and interface -  Clac,MyData --  first letted should be Capital
+// Construstor - Human()
+// varible and method - marks, show(), showMyMarks()
+// constants - PIE, BRAND, MY_DATA
